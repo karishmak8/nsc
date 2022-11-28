eaf5218b1312ae44444bfbe7873b425e2fee5341
karishmak8 <61000172+karishmak8@users.noreply.github.com>
2022-11-28 10:09:39 +0530

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,23 +1,28 @@
-choice=int(input("Enter choice : 1.Encryption 2.Decryption:"))
-if choice==1:
-    pt=input("Enter plain text : ")
-    k=int(input("Enter key value : "))
-    ct=""
-    for i in range(len(pt)):
-        ch=pt[i]
-        if ch.isupper():
-            ct = ct + chr(((ord(ch) + k - 65) % 26) + 65)
-        else:
-            ct = ct + chr(((ord(ch) + k - 97) % 26) + 97)
-    print("Cipher text is : ",ct)
-elif choice==2:
-    ct=input("Enter cipher text : ")
-    k=int(input("Enter key value : "))
-    pt=""
-    for i in range(len(ct)):
-        ch=ct[i]
-        if ch.isupper():
-            pt = pt + chr(((ord(ch) - k - 65) % 26) + 65)
-        else:
-            pt = pt + chr(((ord(ch) - k - 97) % 26) + 97)
-    print("Plain text is : ",pt)
+def bruteForce(ct): 
+    for k in range(1,27):
+        pt="" 
+        for a in ct:
+            if(ord(a)>=65 and ord(a)<=90): 
+                pt+=chr((ord(a)-65-k)%26+65) 
+            if(ord(a)>=97 and ord(a)<=122): 
+                pt+=chr((ord(a)-97-k)%26+97) 
+        print(pt) 
+pt = input("Enter the text: ") 
+ct="" 
+k=int(input("Enter the key: ")) 
+#at sender's side 
+for a in pt:
+    if(ord(a)>=65 and ord(a)<=90):
+        ct+=chr((ord(a)-65+k)%26+65) 
+    if(ord(a)>=97 and ord(a)<=122): 
+        ct+=chr((ord(a)-97+k)%26+97) 
+print("Cypher text: ",ct) 
+#at receiver's side 
+dt="" 
+for a in ct:
+    if(ord(a)>=65 and ord(a)<=90):
+        dt+=chr((ord(a)-65-k)%26+65) 
+    if(ord(a)>=97 and ord(a)<=122): 
+        dt+=chr((ord(a)-97-k)%26+97) 
+print("Plain text: ",dt) 
+bruteForce(ct)
